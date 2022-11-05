e9156936953f63b48f3cb60d1c42cbe6d45c23c4
Ashen Hettiarachchi <ashenirhettiarachchi@gmail.com>
2022-11-06 01:17:39 +0530

    initial commit

---
+++ Backend/App.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require('cors');
+
+const bodyParser = require("body-parser");
+const connectDB = require("./src/config/config");
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json())
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
